3a6c1972373cc04aaa9424c1bd723e850a941e96
FahimKarimi <fahimkarimi2009@gmail.com>
2023-06-08 00:55:38 +0430

    New changes

---
+++ lib/screens/add_measure_screen.dart
@@ -0,0 +1,89 @@
+import 'package:tylor_app/widgets/lower_garment_table.dart';
+import 'package:tylor_app/widgets/measure_text_field.dart';
+import 'package:tylor_app/widgets/upper_garment_table.dart';
+import 'package:flutter/material.dart';
+
+class MeasuresScreen extends StatefulWidget 
+{
+  final bool inferior;
+
+  const MeasuresScreen({super.key, required this.inferior});
+
+  @override
+  State<MeasuresScreen> createState() => _MeasuresScreenState();
+}
+
+class _MeasuresScreenState extends State<MeasuresScreen> 
+{
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController cellnumController = TextEditingController();
+
+  @override
+  void dispose() 
+  {
+    nameController.dispose();
+    cellnumController.dispose();
+    //ControllerList.disposeControllers();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) 
+  {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 240, 255, 225),
+      appBar: AppBar(
+        title: const Text('مشخصات لباس'),
+      ),
+
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(10),
+                decoration: const BoxDecoration(
+                  //color: Colors.lightBlue,
+                  borderRadius: BorderRadius.all(Radius.circular(15))
+                ),
+                child: Column(
+                  children: [
+                    MeasureTextField(
+                      controller: nameController, 
+                      text: 'نام',
+                      keyboardType: TextInputType.text,
+                    ),
+        
+                    const SizedBox(height: 10),
+        
+                    MeasureTextField(
+                      controller: cellnumController,
+                      text: 'شماره تماس',
+                      keyboardType: TextInputType.text,
+                    ),
+                  ],
+                ),
+              ),
+
+              const Padding(padding: EdgeInsets.only(bottom: 15)),
+        
+              const Divider(color: Colors.black, height: 2),
+        
+              widget.inferior 
+              ? Padding(
+                padding: const EdgeInsets.symmetric(vertical: 15),
+                child: LowerGarmentsMeasureTableWidget(nameController: nameController, cellnumController: cellnumController),
+              )
+
+              : Padding(
+                padding: const EdgeInsets.symmetric(vertical: 15),
+                child: UpperGarmentsMeasureTableWidget(nameController: nameController, cellnumController: cellnumController,),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,110 @@
+import 'package:tylor_app/screens/client_list_screen.dart';
+import 'package:tylor_app/screens/motivational_quotes_screen.dart';
+import 'package:tylor_app/widgets/dialog_box.dart';
+import 'package:tylor_app/widgets/measure_buttons.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget 
+{
+  final String title;
+
+  const HomeScreen({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) 
+  {
+
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 169, 245, 222),
+      appBar: AppBar(
+        title: Text(title),
+      ),
+
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    child: Column(
+                      children: const [
+                        Text(
+                          'به سیستم خیاطی خوش آمدید',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+
+                        SizedBox(height: 15),
+                        Text(
+                          'با این سیستم شما میتوانید سفارشات مشتریان تان را به راحتی مدیریت کنید',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 17
+                          ),
+                        ),
+                      ],
+                    )
+                  ),
+
+                  Column(
+                    children: [
+                      MyButton(
+                        text: 'اضافه کردن مشتری',
+                        onPressed: () 
+                        {
+                          showDialog(
+                            context: context, 
+                            builder: (context) 
+                            {
+                              return const DialogBox();
+                            },
+                          );
+                        },
+                      ),
+
+                      const Padding(padding: EdgeInsets.all(10)),
+
+                      MyButton(
+                        text: 'کار ها',
+                        onPressed: () 
+                        {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => const ClientListScreen(),
+                            )
+                          );
+                        },
+                      ),
+
+                      const Padding(padding: EdgeInsets.all(10)),
+                      
+                      MyButton(
+                        text: 'درباره ما',
+                        onPressed: () 
+                        {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => const MotivationalQuotesScreen(),
+                            )
+                          );
+                        },
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
